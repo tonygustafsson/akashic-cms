1ecfe5f13201bf7d9ac3a34a1598d200e318e079
Tony Gustafsson <mail@tonyg.se>
2018-05-01 12:11:38 +0200

    Class based PHP

--- index.php
+++ index.php
@@ -1,46 +1,58 @@
 <?php
-	function getFileContent($file) {
+require_once("settings.php");
+
+class Akashic {
+	public $url_path;
+	public $url_segments;
+	public $settings;
+
+	public function __construct() {
+		$this->settings = new Settings();
+		$this->url_path = str_replace($this->settings->site_path, '', $_SERVER['REQUEST_URI']);
+		$this->url_segments = empty($this->url_path) ? array() : explode('/', $this->url_path);
+
+		if (count($this->url_segments) == 0) {
+			// Start page
+			$content = $this->getFileContent("start.php");
+			echo $this->processTemplateVars($content);
+		}
+		else if (file_exists($this->url_path . ".php")) {
+			// Special page
+			$content = $this->getFileContent($this->url_path . ".php");
+			echo $this->processTemplateVars($content);	
+		}
+		else {
+			$content = $this->getFileContent("404.php");
+			echo $this->processTemplateVars($content);
+		}
+	}
+
+	private function getFileContent($file) {
 		ob_start();
 		include($file);
 		return ob_get_clean();
 	}
 
-	function getTemplate($matches) {
+	private function getTemplate($matches) {
 		$file = $matches[1] . '.php';
 			
 		if (!file_exists($file)) {
 			return 'Template "' . $file . '" does not exists.';
 		}
 		
-		$newContent = getFileContent($file);
+		$newContent = $this->getFileContent($file);
 		
 		if (preg_match('/\[\[(.*)\]\]/i', $newContent)) {
-			return processTemplateVars($newContent);
+			return $this->processTemplateVars($newContent);
 		}
 		
 		return $newContent;
 	}
 	
-	function processTemplateVars($content) {
+	private function processTemplateVars($content) {
 		$includePattern = '/\[\[(.*)\]\]/i';
-		return preg_replace_callback($includePattern, 'getTemplate', $content);
+		return preg_replace_callback($includePattern, array($this, 'getTemplate'), $content);
 	}
+}
 
-	$site_path = '/akashic-cms/';
-	$url_path = str_replace($site_path, '', $_SERVER['REQUEST_URI']);
-	$url_segments = empty($url_path) ? array() : explode('/', $url_path);
-
-	if (count($url_segments) == 0) {
-		// Start page
-		$content = getFileContent("start.php");
-		echo processTemplateVars($content);
-	}
-	else if (file_exists($url_path . ".php")) {
-		// Special page
-		$content = getFileContent($url_path . ".php");
-		echo processTemplateVars($content);	
-	}
-	else {
-		$content = getFileContent("404.php");
-		echo processTemplateVars($content);
-	}
+$akashic = new Akashic();
---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+
+class Settings {
+    public $site_path;
+
+    public function __construct() {
+		$this->site_path = '/akashic-cms/';
+    }
+}
