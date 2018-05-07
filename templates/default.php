54138ce871f8bd6ba274cd769a7f07f72c7d408c
Tony Gustafsson <mail@tonyg.se>
2018-05-07 20:34:21 +0200

    Make default template non ugly

--- templates/default.php
+++ templates/default.php
@@ -1,9 +1,16 @@
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
         <title>Akashic CMS</title>
         <style>
             body {
-                background: #ccc;
+                width: 80%;
+                margin: 0 10%;
+                font-family: 'Bree Serif', serif;
             }
         </style>
     </head>
